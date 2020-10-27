1f5b2f983f8967c0dc92d0421cec886b3e48aa8e
Dongjae10 <movementj10@gmail.com>
2020-10-27 17:53:32 +0900

    Stack에 대하여...

--- src/Java2020_10_27/PracticeStack.java
+++ src/Java2020_10_27/PracticeStack.java
@@ -1,4 +1,20 @@
 package Java2020_10_27;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class PracticeStack {
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+
+        String numbers[] = {"1", "2", "3"};
+
+        for(String a : numbers)
+            stack.add(a);
+
+        Iterator<String> iterator = stack.iterator();
+        while(iterator.hasNext())
+            System.out.println(stack.pop());
+    }
 }
