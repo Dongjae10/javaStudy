09d6b27f9fa87bc6a0667d603dcf28b86c21296f
Dongjae10 <movementj10@gmail.com>
2020-11-03 18:22:38 +0900

    예외처리 실습2

---
+++ src/Java2020_11_03/PracticeExceptionV2.java
@@ -0,0 +1,25 @@
+package Java2020_11_03;
+
+class Student{
+    int grade;
+
+    void setGrade(int grade) throws Exception{
+        if(grade < 1 || grade > 6){
+            Exception ex = new Exception("학년 에러");
+            throw ex;
+        }
+        this.grade = grade;
+    }
+}
+public class PracticeExceptionV2 {
+    public static void main(String[] args) {
+        Student student = new Student();
+        try {
+            student.setGrade(0);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
