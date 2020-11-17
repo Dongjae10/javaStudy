12eb28d94ed04d47357fd768597da0059b8ab87c
Dongjae10 <movementj10@gmail.com>
2020-11-17 16:17:31 +0900

    예외처리에 대하여3...

--- src/Java2020_11_10/PracticeException3.java
+++ src/Java2020_11_10/PracticeException3.java
@@ -1,2 +1,38 @@
-package Java2020_11_10;public class PracticeException3 {
+package Java2020_11_10;
+
+public class PracticeException3 {
+    public static void main(String[] args) {
+        try {
+            try {
+                makePerson();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            try {
+                int a = Integer.parseInt("10asd");
+                System.out.println(a);
+            } catch (NumberFormatException e) {
+                System.out.println("사용자가 잘못 입력함 - 사용자한테 다시 요청");
+            }
+        }catch (OutOfMemoryError e){
+            System.out.println("Out of Memory Error");
+        }
+
+    }
+    static void makePerson() throws Exception {
+        PersonE p = new PersonE();
+        p.setGrade(7);
+    }
 }
+
+class PersonE {
+    int age;
+    int grade;
+    void setGrade(int grade) throws Exception{
+        if(grade < 1 || grade > 6){
+            Exception e = new Exception ("학년 에러");
+            throw e;
+        }
+        this.grade = grade;
+        }
+        }
