693c5938bae4398f6cc188df905f23752061a759
Dongjae10 <movementj10@gmail.com>
2020-11-24 18:08:53 +0900

    멀티 쓰레드에 대하여3...

--- src/Java2020_11_24/PracticeMultiThread03.java
+++ src/Java2020_11_24/PracticeMultiThread03.java
@@ -1,2 +1,41 @@
-package Java2020_11_24;public class PracticeMultiThread03 {
+package Java2020_11_24;
+
+public class PracticeMultiThread03 {
+    public static void main(String[] args) {
+        SharedBoard board = new SharedBoard();
+        StudentThread thread = new StudentThread("KYW",board);
+        StudentThread thread2 = new StudentThread("LWG",board);
+        thread.start();
+        thread2.start();
+    }
+}
+
+class SharedBoard {
+    private int sum = 0;
+    int n = 0;
+    public synchronized void add(){
+        n = sum ;
+        n += 10;
+        sum = n;
+        System.out.println(Thread.currentThread().getId()+ " , "+ Thread.currentThread().getName() + " , " + sum);
+    }
+}
+
+class StudentThread extends Thread {
+    private SharedBoard board;
+    public StudentThread (String name, SharedBoard board) {
+        super(name);
+        this.board = board;
+    }
+
+    public void run(){
+        for (int i = 0; i < 10; i++){
+            try{
+                board.add();
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
